44c53f1f845c70830ea4a1e73a0027ddc0ce2d6a
Raiyan <kazimdraiyan2006@gmail.com>
2025-06-13 04:13:35 +0600

    add player, platforms, simple movement, boundary checking

--- iMain.cpp
+++ iMain.cpp
@@ -1,80 +1,107 @@
 #include "iGraphics.h"
 
-/*
-function iDraw() is called again and again by the system.
-*/
+// TODO: Try to use object oriented programming.
+// TODO: Try to add Dvorak keyboard support.
+
+#define WIDTH 1920
+#define HEIGHT 1080
+#define TITLE "Platformer Game"
+
+// TODO: Check out how these constants work and be optimized.
+#define LEFT 0x0001
+#define RIGHT 0x0002
+#define UP 0x0003
+#define DOWN 0x0004
+
+bool firstDraw = true;
+
+
+// Player represented as an array: {x, y, width, height}
+double player[4] = {
+    WIDTH / 9.0, 150, 50, 50};
+
+// Platforms represented as an array of arrays: {x, y, width, height}
+double platforms[][4] = {
+    {WIDTH / 9.0, 100, WIDTH / 3.0, 50},    // Platform 1
+    {WIDTH * 5.0 / 9, 100, WIDTH / 3.0, 50} // Platform 2
+};
+
+int touchingBoundary()
+{
+    if (player[0] <= 0)
+        return LEFT;
+    else if (player[0] + player[2] >= WIDTH)
+        return RIGHT;
+    else if (player[1] <= 0)
+        return DOWN;
+    else if (player[1] + player[3] >= HEIGHT)
+        return UP;
+    return 0; // Not touching any boundary
+}
+
+int blockedSide()
+{
+    return touchingBoundary();
+}
+
 void iDraw()
 {
-    // place your drawing codes here
+    if (firstDraw)
+    {
+        glutFullScreen();
+        firstDraw = false;
+    }
+
     iClear();
-    iText(140, 180, "Hello World");
+
+    iSetColor(50, 50, 50);
+    iFilledRectangle(0, 0, WIDTH, HEIGHT); // Background
+
+    // Two platforms
+    iSetColor(0, 0, 0);
+    iFilledRectangle(platforms[0][0], platforms[0][1], platforms[0][2], platforms[0][3]); // Platform 1
+    iFilledRectangle(platforms[1][0], platforms[1][1], platforms[1][2], platforms[1][3]); // Platform 2
+
+    // Player
+    iSetColor(250, 0, 0);
+    iFilledRectangle(player[0], player[1], player[2], player[3]);
 }
 
-/*
-function iMouseMove() is called when the user moves the mouse.
-(mx, my) is the position where the mouse pointer is.
-*/
-void iMouseMove(int mx, int my)
-{
-    // place your codes here
-}
-
-/*
-function iMouseDrag() is called when the user presses and drags the mouse.
-(mx, my) is the position where the mouse pointer is.
-*/
-void iMouseDrag(int mx, int my)
-{
-    // place your codes here
-}
-
-/*
-function iMouse() is called when the user presses/releases the mouse.
-(mx, my) is the position where the mouse pointer is.
-*/
-void iMouse(int button, int state, int mx, int my)
-{
-    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
-    {
-        // place your codes here
-    }
-    if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN)
-    {
-        // place your codes here
-    }
-}
-
-/*
-function iMouseWheel() is called when the user scrolls the mouse wheel.
-dir = 1 for up, -1 for down.
-*/
-void iMouseWheel(int dir, int mx, int my)
-{
-    // place your code here
-}
-
-/*
-function iKeyboard() is called whenever the user hits a key in keyboard.
-key- holds the ASCII value of the key pressed.
-*/
 void iKeyboard(unsigned char key)
 {
     switch (key)
     {
-    case 'q':
-        // do something with 'q'
+    // TODO: Implement the logic where the player is currently not blocked, but will be blocked if it moves, then it will move only the available space.
+    case 'a':
+        if (blockedSide() != LEFT)
+        {
+            player[0] -= 10;
+        }
         break;
-    // place your codes for other keys here
+    case 'd':
+        if (blockedSide() != RIGHT)
+        {
+            player[0] += 10;
+        }
+        break;
+    case 'w':
+        if (blockedSide() != UP)
+        {
+            player[1] += 10;
+        }
+        break;
+    case 's':
+        if (blockedSide() != DOWN)
+        {
+            player[1] -= 10;
+        }
+        break;
     default:
         break;
     }
 }
 
-/*
-function iSpecialKeyboard() is called whenver user hits special keys likefunction
-keys, home, end, pg up, pg down, arraows etc. you have to use
-appropriate constants to detect them. A list is:
-GLUT_KEY_F1, GLUT_KEY_F2, GLUT_KEY_F3, GLUT_KEY_F4, GLUT_KEY_F5, GLUT_KEY_F6,
+/* GLUT_KEY_F1, GLUT_KEY_F2, GLUT_KEY_F3, GLUT_KEY_F4, GLUT_KEY_F5, GLUT_KEY_F6,
 GLUT_KEY_F7, GLUT_KEY_F8, GLUT_KEY_F9, GLUT_KEY_F10, GLUT_KEY_F11,
 GLUT_KEY_F12, GLUT_KEY_LEFT, GLUT_KEY_UP, GLUT_KEY_RIGHT, GLUT_KEY_DOWN,
 GLUT_KEY_PAGE_UP, GLUT_KEY_PAGE_DOWN, GLUT_KEY_HOME, GLUT_KEY_END,
@@ -83,19 +110,32 @@ void iSpecialKeyboard(unsigned char key)
 {
     switch (key)
     {
-    case GLUT_KEY_END:
-        // do something
-        break;
-    // place your codes for other keys here
     default:
         break;
     }
 }
 
+void iMouseMove(int mx, int my)
+{
+}
+
+void iMouseDrag(int mx, int my)
+{
+}
+
+void iMouse(int button, int state, int mx, int my)
+{
+}
+
+void iMouseWheel(int dir, int mx, int my)
+{
+}
+
 int main(int argc, char *argv[])
 {
     glutInit(&argc, argv);
-    // place your own initialization codes here.
-    iInitialize(400, 400, "demooo");
+
+    iInitialize(WIDTH, HEIGHT, TITLE);
+
     return 0;
 }
