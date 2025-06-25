dec77fc8ecf8ce6be735f1265a7d21abc954c7c0
Raiyan <kazimdraiyan2006@gmail.com>
2025-06-25 07:22:20 +0600

    load multiple levels, add coins

--- iMain.cpp
+++ iMain.cpp
@@ -22,8 +22,11 @@
 #define OPTIONS_PAGE 0x0004
 
 int currentPage = MENU_PAGE;
+int currentLevel = 0;
 
 void drawMenuPage();
+void drawLevelsPage();
+void drawCoinCount();
 
 Image background_image;
 Image tile;
@@ -35,11 +38,40 @@ int hoverRectangleHeight = 50;
 bool firstDraw = true;
 bool blockedSides[4] = {false}; // Left, Right, Up, Down
 double t = 0;
+int collectedCoins = 0;
 
 // Player represented as an array: {x, y, width, height}
 double player[4] = {WIDTH / 9.0, 150, 30, 30};
 
 char tiles[ROWS][COLUMNS + 1] = {};
+
+void loadLevel(int level)
+{
+    // Loading levels
+    FILE *file_pointer;
+    char filename[20];
+    sprintf(filename, "levels/level%d.txt", level);
+    file_pointer = fopen(filename, "r");
+    char discard[2];
+    if (file_pointer != NULL)
+    {
+        for (int row = 1; row <= ROWS; row++)
+        {
+            fgets(tiles[row - 1], COLUMNS + 1, file_pointer); // +1 for null terminator.
+            fgets(discard, 1 + 1, file_pointer);              // Read and discard the newline character. +1 for null terminator.
+        }
+
+        for (int i = 0; i < ROWS; i++)
+        {
+            printf("%d: %s\n", i, tiles[i]);
+        }
+    }
+    else
+    {
+        printf("Failed to load level.");
+    }
+    fclose(file_pointer);
+}
 
 bool doesTouchLeftBoundary(double x)
 {
@@ -139,6 +171,10 @@ void iDraw()
 
         iLoadImage(&tile, "assets/images/tile.png");
         iResizeImage(&tile, WIDTH / COLUMNS, HEIGHT / ROWS);
+
+        currentLevel = 1;
+        loadLevel(currentLevel);
+
         firstDraw = false;
     }
 
@@ -157,6 +193,12 @@ void iDraw()
             {
                 iShowLoadedImage(col * (WIDTH / COLUMNS), (ROWS - row - 1) * (HEIGHT / ROWS), &tile);
             }
+            else if (tiles[row][col] == 'O')
+            {
+                // Coin
+                iSetColor(255, 215, 0); // Gold color
+                iFilledCircle(col * (WIDTH / COLUMNS) + (WIDTH / COLUMNS) / 2, (ROWS - row - 1) * (HEIGHT / ROWS) + (HEIGHT / ROWS) / 2, 10, 100);
+            }
         }
     }
 
@@ -165,6 +207,27 @@ void iDraw()
     iFilledRectangle(player[0], player[1], player[2], player[3]);
 
     updateBlockedSides();
+
+    // Collect coins
+    for (int row = 0; row < ROWS; row++)  
+    {
+        for (int col = 0; col < COLUMNS; col++)
+        {
+            if (tiles[row][col] == 'O')
+            {
+                double coinX = col * (WIDTH / COLUMNS) + (WIDTH / COLUMNS) / 2;
+                double coinY = (ROWS - row - 1) * (HEIGHT / ROWS) + (HEIGHT / ROWS) / 2;
+                if (player[0] < coinX + 10 && player[0] + player[2] > coinX - 10 &&
+                    player[1] < coinY + 10 && player[1] + player[3] > coinY - 10)
+                {
+                    collectedCoins++;
+                    tiles[row][col] = ' ';
+                }
+            }
+        }
+    }
+
+    drawCoinCount();
 
     // Apply gravity effect
     t++;
@@ -189,6 +252,7 @@ void iDraw()
     }
     else if (currentPage == LEVELS_PAGE)
     {
+        drawLevelsPage();
     }
     else if (currentPage == HIGH_SCORES_PAGE)
     {
@@ -217,7 +281,7 @@ void iKeyboard(unsigned char key)
                 player[0] = 0; // Prevent going out of bounds
             }
             else
-                player[0] -= 10;
+                player[0] -= 25;
         }
         break;
     case 'd':
@@ -228,7 +292,7 @@ void iKeyboard(unsigned char key)
                 player[0] = WIDTH - player[2]; // Prevent going out of bounds
             }
             else
-                player[0] += 10;
+                player[0] += 25;
         }
         break;
     case 27: // Escape key
@@ -237,6 +301,14 @@ void iKeyboard(unsigned char key)
     default:
         break;
     }
+}
+
+void drawCoinCount()
+{
+    iSetColor(0, 0, 0);
+    char coinText[50];
+    sprintf(coinText, "Coins: %d", collectedCoins);
+    iTextAdvanced(WIDTH - 200, HEIGHT - 50, coinText, 0.3, 2.5, GLUT_STROKE_ROMAN);
 }
 
 void drawMenuPage()
@@ -263,6 +335,29 @@ void drawMenuPage()
     }
 }
 
+void drawLevelsPage()
+{
+    iClear();
+    iSetColor(255, 255, 255);
+
+    iShowLoadedImage(0, 0, &background_image);
+
+    iSetColor(0, 0, 0);
+    iTextAdvanced(WIDTH / 2 - 100, HEIGHT / 2 + 200, "Levels", 0.5, 5.0, GLUT_STROKE_ROMAN);
+    iTextAdvanced(WIDTH / 2 - 100, HEIGHT / 2 + 50, "Level 1", 0.3, 2.5, GLUT_STROKE_ROMAN);
+    iTextAdvanced(WIDTH / 2 - 100, HEIGHT / 2 + 0, "Level 2", 0.3, 2.5, GLUT_STROKE_ROMAN);
+    iTextAdvanced(WIDTH / 2 - 100, HEIGHT / 2 - 50, "Level 3", 0.3, 2.5, GLUT_STROKE_ROMAN);
+    iTextAdvanced(WIDTH / 2 - 100, HEIGHT / 2 - 100, "Level 4", 0.3, 2.5, GLUT_STROKE_ROMAN);
+    iTextAdvanced(WIDTH / 2 - 100, HEIGHT / 2 - 150, "Level 5", 0.3, 2.5, GLUT_STROKE_ROMAN);
+
+    // Draw hover effect rectangles
+    if (hoverRectangleY != -1)
+    {
+        iSetTransparentColor(100, 100, 100, 0.25); // TODO: Store the colors somewhere instead of hardcoding them.
+        iFilledRectangle(0, hoverRectangleY, WIDTH, hoverRectangleHeight);
+    }
+}
+
 // GLUT_KEY_F1, GLUT_KEY_F2, GLUT_KEY_F3, GLUT_KEY_F4, GLUT_KEY_F5, GLUT_KEY_F6, GLUT_KEY_F7, GLUT_KEY_F8, GLUT_KEY_F9, GLUT_KEY_F10, GLUT_KEY_F11, GLUT_KEY_F12, GLUT_KEY_LEFT, GLUT_KEY_UP, GLUT_KEY_RIGHT, GLUT_KEY_DOWN, GLUT_KEY_PAGE_UP, GLUT_KEY_PAGE_DOWN, GLUT_KEY_HOME, GLUT_KEY_END, GLUT_KEY_INSERT
 void iSpecialKeyboard(unsigned char key)
 {
@@ -275,37 +370,71 @@ void iSpecialKeyboard(unsigned char key)
 
 void iMouseMove(int mx, int my)
 {
-    if (currentPage != MENU_PAGE)
-        return;
-    // Hover effect
-    if (my >= hoverRectangleYs[0] && my <= hoverRectangleYs[0] + hoverRectangleHeight)
+    if (currentPage == MENU_PAGE)
     {
-        // Resume button
-        hoverRectangleY = hoverRectangleYs[0];
+        // Hover effect
+        if (my >= hoverRectangleYs[0] && my <= hoverRectangleYs[0] + hoverRectangleHeight)
+        {
+            // Resume button
+            hoverRectangleY = hoverRectangleYs[0];
+        }
+        else if (my >= hoverRectangleYs[1] && my <= hoverRectangleYs[1] + hoverRectangleHeight)
+        {
+            // Levels button
+            hoverRectangleY = hoverRectangleYs[1];
+        }
+        else if (my >= hoverRectangleYs[2] && my <= hoverRectangleYs[2] + hoverRectangleHeight)
+        {
+            // High Scores button
+            hoverRectangleY = hoverRectangleYs[2];
+        }
+        else if (my >= hoverRectangleYs[3] && my <= hoverRectangleYs[3] + hoverRectangleHeight)
+        {
+            // Options button
+            hoverRectangleY = hoverRectangleYs[3];
+        }
+        else if (my >= hoverRectangleYs[4] && my <= hoverRectangleYs[4] + hoverRectangleHeight)
+        {
+            // Exit button
+            hoverRectangleY = hoverRectangleYs[4];
+        }
+        else
+        {
+            hoverRectangleY = -1;
+        }
     }
-    else if (my >= hoverRectangleYs[1] && my <= hoverRectangleYs[1] + hoverRectangleHeight)
+    else if (currentPage == LEVELS_PAGE)
     {
-        // Levels button
-        hoverRectangleY = hoverRectangleYs[1];
-    }
-    else if (my >= hoverRectangleYs[2] && my <= hoverRectangleYs[2] + hoverRectangleHeight)
-    {
-        // High Scores button
-        hoverRectangleY = hoverRectangleYs[2];
-    }
-    else if (my >= hoverRectangleYs[3] && my <= hoverRectangleYs[3] + hoverRectangleHeight)
-    {
-        // Options button
-        hoverRectangleY = hoverRectangleYs[3];
-    }
-    else if (my >= hoverRectangleYs[4] && my <= hoverRectangleYs[4] + hoverRectangleHeight)
-    {
-        // Exit button
-        hoverRectangleY = hoverRectangleYs[4];
-    }
-    else
-    {
-        hoverRectangleY = -1;
+        // Hover effect for levels
+        if (my >= hoverRectangleYs[0] && my <= hoverRectangleYs[0] + hoverRectangleHeight)
+        {
+            // Level 1 button
+            hoverRectangleY = hoverRectangleYs[0];
+        }
+        else if (my >= hoverRectangleYs[1] && my <= hoverRectangleYs[1] + hoverRectangleHeight)
+        {
+            // Level 2 button
+            hoverRectangleY = hoverRectangleYs[1];
+        }
+        else if (my >= hoverRectangleYs[2] && my <= hoverRectangleYs[2] + hoverRectangleHeight)
+        {
+            // Level 3 button
+            hoverRectangleY = hoverRectangleYs[2];
+        }
+        else if (my >= hoverRectangleYs[3] && my <= hoverRectangleYs[3] + hoverRectangleHeight)
+        {
+            // Level 4 button
+            hoverRectangleY = hoverRectangleYs[3];
+        }
+        else if (my >= hoverRectangleYs[4] && my <= hoverRectangleYs[4] + hoverRectangleHeight)
+        {
+            // Level 5 button
+            hoverRectangleY = hoverRectangleYs[4];
+        }
+        else
+        {
+            hoverRectangleY = -1;
+        }
     }
 }
 
@@ -315,11 +444,9 @@ void iMouseDrag(int mx, int my)
 
 void iMouse(int button, int state, int mx, int my)
 {
-    if (currentPage != MENU_PAGE)
-        return;
     // TODO: Add click effect?
     // Button click handling
-    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN && currentPage == MENU_PAGE)
     {
         if (my >= hoverRectangleYs[0] && my <= hoverRectangleYs[0] + hoverRectangleHeight)
         {
@@ -347,6 +474,44 @@ void iMouse(int button, int state, int mx, int my)
             exit(0);
         }
     }
+    else if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN && currentPage == LEVELS_PAGE)
+    {
+        if (my >= hoverRectangleYs[0] && my <= hoverRectangleYs[0] + hoverRectangleHeight)
+        {
+            printf("Level 1 button clicked\n");
+            currentLevel = 1;
+            currentPage = GAME_PAGE;
+        }
+        else if (my >= hoverRectangleYs[1] && my <= hoverRectangleYs[1] + hoverRectangleHeight)
+        {
+            printf("Level 2 button clicked\n");
+            currentLevel = 2;
+            currentPage = GAME_PAGE;
+        }
+        else if (my >= hoverRectangleYs[2] && my <= hoverRectangleYs[2] + hoverRectangleHeight)
+        {
+            printf("Level 3 button clicked\n");
+            currentLevel = 3;
+            currentPage = GAME_PAGE;
+        }
+        else if (my >= hoverRectangleYs[3] && my <= hoverRectangleYs[3] + hoverRectangleHeight)
+        {
+            printf("Level 4 button clicked\n");
+            currentLevel = 4;
+            currentPage = GAME_PAGE;
+        }
+        else if (my >= hoverRectangleYs[4] && my <= hoverRectangleYs[4] + hoverRectangleHeight)
+        {
+            printf("Level 5 button clicked\n");
+            currentLevel = 5;
+            currentPage = GAME_PAGE;
+        }
+        collectedCoins = 0;
+        loadLevel(currentLevel);
+    }
+    else
+    {
+    }
 }
 
 void iMouseWheel(int dir, int mx, int my)
@@ -355,29 +520,6 @@ void iMouseWheel(int dir, int mx, int my)
 
 int main(int argc, char *argv[])
 {
-    // Loading levels
-    FILE *file_pointer;
-    file_pointer = fopen("levels/level1.txt", "r");
-    char discard[2];
-    if (file_pointer != NULL)
-    {
-        for (int row = 1; row <= ROWS; row++)
-        {
-            fgets(tiles[row - 1], COLUMNS + 1, file_pointer); // +1 for null terminator.
-            fgets(discard, 1 + 1, file_pointer);              // Read and discard the newline character. +1 for null terminator.
-        }
-
-        for (int i = 0; i < ROWS; i++)
-        {
-            printf("%d: %s\n", i, tiles[i]);
-        }
-    }
-    else
-    {
-        printf("Failed to load level.");
-    }
-    fclose(file_pointer);
-
     for (int i = 1; i < 5; i++)
     {
         hoverRectangleYs[i] = hoverRectangleYs[i - 1] - hoverRectangleHeight;
